5686b0194ef2e4448b0a5a68afa0cce0e4f7aaa0
Prince Hatty <72368504+PrinceHatty@users.noreply.github.com>
2021-12-01 13:11:21 -0500

    e

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ let scrollpos = 0;
 let delay = 0;
 
 scene.on("update", e => {
-    scrollpos = e.scrollPos / 1000;
+    scrollpos = e.scrollPos / 500;
 })
 setInterval(() => {
     delay +=(scrollpos - delay)*accelamount;
